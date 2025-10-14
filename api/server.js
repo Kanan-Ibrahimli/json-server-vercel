9df4670e0e39958fa4f978f915815dbf4b9a553e
Kanan Ibrahimli <kanan.ibrahimli@nixs.com>
2025-10-14 17:40:32 +0200

    add json-server-auth

--- api/server.js
+++ api/server.js
@@ -1,8 +1,11 @@
 const jsonServer = require('json-server');
+const cors = require('cors'); 
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const auth = require('json-server-auth');
+
+server.use(cors()); 
 
 server.use(middlewares);
 server.db = router.db;
@@ -14,3 +17,4 @@ server.use(router);
 server.listen(3000, () => {
   console.log('JSON Server with Auth is running');
 });
+
